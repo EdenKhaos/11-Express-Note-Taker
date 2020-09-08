ea113444ace882c4d965d612a57549518d55cbaf
Heather Cooper <raven.dantes@ymail.com>
2020-09-08 14:29:59 -0600

    add oruts folder with htmlroute file

---
+++ routes/htmlRoute.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+// routing
+module.exports = function(app) {
+  // GET Requests
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  // link to css file
+  app.get("/styles", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/css/styles.css"));
+  });
+
+  // if no matching route is found default to home page
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
